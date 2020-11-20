666c0a466f117d121189b85aec617c09c3006583
Gowtham <gowtham4n@gmail.com>
2020-11-20 20:46:29 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+BOARD = [[]]
+
+
+def drawEmptyBoard(width, height):
+    global BOARD
+    BOARD = [["." for x in range(width)] for y in range(height)]
+
+
+def replacePoint(x, y):
+    BOARD[x][y] = "#"
+
+
+def drawInFormat(boardArr):
+    for i in boardArr:
+        print(i)
+    print("-----------------")
+
+
+
+
+if __name__ == '__main__':
+    drawEmptyBoard(5, 6)
+    drawInFormat(BOARD)
+    replacePoint(1, 2)
+    drawInFormat(BOARD)
